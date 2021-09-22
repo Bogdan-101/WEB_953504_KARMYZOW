852be84b410c53dcb46f7f4cfb3da281e80ce06d
Богдан Кармызов <big_bogdan@rambler.ru>
2021-09-22 19:09:12 +0300

    feat: added first and second labs

---
+++ WEB_953504_KARMYZOW/Models/ListDemoModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEB_953504_KARMYZOW.Models
+{
+    public class ListDemoModel
+    {
+        public int ListItemValue { get; set; }
+        public string ListItemText { get; set; }
+    }
+}
