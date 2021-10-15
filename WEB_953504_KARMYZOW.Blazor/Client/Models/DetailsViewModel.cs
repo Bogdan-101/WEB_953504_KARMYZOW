3430f00d01e9490ce85c1a68acb9c7133dbd31f6
Богдан Кармызов <big_bogdan@rambler.ru>
2021-10-15 21:14:31 +0300

    feat: 8th lab

---
+++ WEB_953504_KARMYZOW.Blazor/Client/Models/DetailsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace WEB_953504_KARMYZOW.Blazor.Client.Models
+{
+    public class DetailsViewModel
+    {
+        [JsonPropertyName("Name")]
+        public string Name { get; set; } // название блюда 
+        [JsonPropertyName("description")]
+        public string Description { get; set; } // описание блюда 
+        [JsonPropertyName("rating")]
+        public int Rating { get; set; } // кол. калорий на порцию 
+        [JsonPropertyName("image")]
+        public string Image { get; set; } // имя файла изображения    
+    }
+}
